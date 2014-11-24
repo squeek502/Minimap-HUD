8602c9d5d1f6bec584be89dcb250fc49617f88f2
squeek <squeek502@hotmail.com>
2014-11-23 18:31:40 -0800

    Reinstate the zero-time task to avoid a crash on load  * This still seems to exist (can reproduce it by starting the game, hosting a server, disconnecting, and then trying to rehost)

--- modmain.lua
+++ modmain.lua
@@ -61,70 +61,76 @@ end
 
 local function AddMiniMap(controls)
 
-	-- add the minimap widget and set its position
-	local MiniMapWidget = require "widgets/minimapwidget"
+	-- for some reason, without this the game would crash without an error when calling controls.topright_root:AddChild
+	-- too lazy to track down the cause, so just using this workaround
+	controls.inst:DoTaskInTime( 0, function()
 
-	controls.minimap_small = controls.top_root:AddChild( MiniMapWidget( mapscale ) )
-	local screensize = {GLOBAL.TheSim:GetScreenSize()}
-	PositionMiniMap(controls, screensize)
+		-- add the minimap widget and set its position
+		local MiniMapWidget = require "widgets/minimapwidget"
 
-	local OnUpdate_base = controls.OnUpdate
-	controls.OnUpdate = function(self, dt)
-		local curscreensize = {GLOBAL.TheSim:GetScreenSize()}
-		if curscreensize[1] ~= screensize[1] or curscreensize[2] ~= screensize[2] then
-			PositionMiniMap(controls, curscreensize)
-			screensize = curscreensize
-		end
-	end
+		controls.minimap_small = controls.top_root:AddChild( MiniMapWidget( mapscale ) )
+		local screensize = {GLOBAL.TheSim:GetScreenSize()}
+		PositionMiniMap(controls, screensize)
 
-	-- show and hide the minimap whenever the map gets toggled
-	local ToggleMap_base = controls.ToggleMap
-	controls.ToggleMap = function( self )
-		local wasvisible = controls.minimap_small:IsVisible()
-
-		if wasvisible then
-			controls.minimap_small:Hide()
+		local OnUpdate_base = controls.OnUpdate
+		controls.OnUpdate = function(self, dt)
+			local curscreensize = {GLOBAL.TheSim:GetScreenSize()}
+			if curscreensize[1] ~= screensize[1] or curscreensize[2] ~= screensize[2] then
+				PositionMiniMap(controls, curscreensize)
+				screensize = curscreensize
+			end
 		end
 
-		ToggleMap_base( self )
+		-- show and hide the minimap whenever the map gets toggled
+		local ToggleMap_base = controls.ToggleMap
+		controls.ToggleMap = function( self )
+			local wasvisible = controls.minimap_small:IsVisible()
 
-		if not wasvisible then
-			controls.minimap_small:Show()
-		end
-	end
+			if wasvisible then
+				controls.minimap_small:Hide()
+			end
 
-	-- special case: ToggleMap gets bypassed when the map gets hidden while on the map screen
-	local MapScreen = require "screens/mapscreen"
+			ToggleMap_base( self )
 
-	MapScreen_OnControl_base = MapScreen.OnControl
-	MapScreen.OnControl = function( self, control, down )
-		local ret = MapScreen_OnControl_base(self, control, down)
-
-		if ret and control == GLOBAL.CONTROL_MAP then
-			controls.minimap_small:Show()
+			if not wasvisible then
+				controls.minimap_small:Show()
+			end
 		end
 
-		return ret
-	end
+		-- special case: ToggleMap gets bypassed when the map gets hidden while on the map screen
+		local MapScreen = require "screens/mapscreen"
 
-	-- keep track of zooming while on the map screen
-	local MapWidget = require "widgets/mapwidget"
+		MapScreen_OnControl_base = MapScreen.OnControl
+		MapScreen.OnControl = function( self, control, down )
+			local ret = MapScreen_OnControl_base(self, control, down)
 
-	MapWidget_OnZoomIn_base = MapWidget.OnZoomIn
-	MapWidget.OnZoomIn = function(self)
-		MapWidget_OnZoomIn_base( self )
-		if self.shown then
-			controls.minimap_small.mapscreenzoom = math.max(0,controls.minimap_small.mapscreenzoom-1)
+			if ret and control == GLOBAL.CONTROL_MAP then
+				controls.minimap_small:Show()
+			end
+
+			return ret
 		end
-	end
 
-	MapWidget_OnZoomOut_base = MapWidget.OnZoomOut
-	MapWidget.OnZoomOut = function(self)
-		MapWidget_OnZoomOut_base( self )
-		if self.shown then
-			controls.minimap_small.mapscreenzoom = controls.minimap_small.mapscreenzoom+1
+		-- keep track of zooming while on the map screen
+		local MapWidget = require "widgets/mapwidget"
+
+		MapWidget_OnZoomIn_base = MapWidget.OnZoomIn
+		MapWidget.OnZoomIn = function(self)
+			MapWidget_OnZoomIn_base( self )
+			if self.shown then
+				controls.minimap_small.mapscreenzoom = math.max(0,controls.minimap_small.mapscreenzoom-1)
+			end
 		end
-	end
+
+		MapWidget_OnZoomOut_base = MapWidget.OnZoomOut
+		MapWidget.OnZoomOut = function(self)
+			MapWidget_OnZoomOut_base( self )
+			if self.shown then
+				controls.minimap_small.mapscreenzoom = controls.minimap_small.mapscreenzoom+1
+			end
+		end
+
+	end)
 
 end
 
