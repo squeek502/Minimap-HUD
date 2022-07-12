1d4129a68bd686f6283646f8cb12fbc55204b297
Ryan Liptak <squeek502@hotmail.com>
2022-07-12 16:02:52 -0700

    Bump mod version to 1.0.9

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "Minimap HUD"
 description = "Adds a minimap to the HUD"
 author = "squeek"
-version = "1.0.8"
+version = "1.0.9"
 forumthread = "/files/file/352-minimap-hud/"
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
